385dc3750e001a53318ef06821a024bc3a57d0df
Raphael Lehmann <raphael@rleh.de>
2015-09-16 15:42:39 +0200

    viewer.py: add duration, output_filename and skip_frames parameters; import and call animate_higgs_peak() from animatehiggspeak.py

--- viewer.py
+++ viewer.py
@@ -5,12 +5,16 @@ and starts the other program components.
 
 import argparse
 import ROOT
+from animatehiggspeak import animate_higgs_peak
 
 # create parser, add arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("-tb", "--tangens_beta", required=True, help="tangens beta value", type=float)
 parser.add_argument("-ma", "--m_A_range", required=True, help="m_A range to loop trough (minimum-maximum)", type=str)
 parser.add_argument("-rfn", "--root_file_name", required=True, help="name of root file to use", type=str)
+parser.add_argument("-d", "--duration", required=True, help="animated GIF duration in milliseconds", type=int)
+parser.add_argument("-o", "--output_filename", required=False, help="GIF output filename", type=str)
+parser.add_argument("-s", "--skip_frames", required=False, help="only render every nth frame (default=1)", type=int)
 parser.add_argument("-Hb", "--higgs_boson", required=True, help="disintegrating Higgs boson", choices=["h", "H", "A"])
 args = parser.parse_args()
 
@@ -23,6 +27,9 @@ ma_max = int(ma_max_str)
 root_file_name = args.root_file_name
 tan_beta = args.tangens_beta
 higgs_boson = args.higgs_boson
+output_filename = args.output_filename
+skip_frames = args.skip_frames
+duration = args.duration
 
 # construct dataset name
 dataset_mass_name = "m_" + higgs_boson
@@ -48,4 +55,12 @@ for i in range(ma_min, ma_max, 1):
     print(values_mass[j])
     print(values_width[j])
 
-
+if output_filename is not None and skip_frames is not None:
+    animate_higgs_peak(values_mass, values_width, mA_list, higgs_boson, duration=duration, skipframes=skip_frames,
+                       filename=output_filename)
+elif output_filename is not None:
+    animate_higgs_peak(values_mass, values_width, mA_list, higgs_boson, duration=duration, filename=output_filename)
+elif skip_frames is not None:
+    animate_higgs_peak(values_mass, values_width, mA_list, higgs_boson, duration=duration, skipframes=skip_frames)
+else:
+    animate_higgs_peak(values_mass, values_width, mA_list, higgs_boson, duration=duration)
