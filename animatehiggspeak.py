8ef64746783c7f528dca990d888b574851b3494f
Raphael Lehmann <raphael@rleh.de>
2015-09-16 15:01:50 +0200

    fixed animated breit wigner pdf plot;
    breit winger normalization is incorrect

--- animatehiggspeak.py
+++ animatehiggspeak.py
@@ -3,58 +3,70 @@ import os
 import math
 
 
-def animate_higgs_peak(m_boson, width_boson, mA_min, mA_max, filename="animation.gif", duration=5000):
+def animate_higgs_peak(m_boson, width_boson, m_A, higgs_boson, filename="animation.gif", duration=5000, skipframes=1):
     # remove gif file (if present)
     try:
         os.remove(filename)
     except OSError:
         pass
 
-    x_min = mA_min
-    x_max = mA_max
+    # x axis range (enlarged by 5 (?))
+    x_min = int(min(m_boson)) - 5
+    x_max = int(max(m_boson)) + 5
 
-    # one interval/frame per data set
-    x_width = x_max - x_min
+    # debug output
+    print "x_min =", x_min, "x_max =", x_max
 
     # calculate delay time per frame
-    animation_delay = math.ceil(duration / x_width)
+    # one interval/frame per data set
+    animation_delay = math.ceil(duration * skipframes / len(m_boson))
 
     width = ROOT.RooRealVar("width", "width", 0)
     mean = ROOT.RooRealVar("mean", "mean", 0)
 
     rf = ROOT.RooFit
 
-    x = ROOT.RooRealVar("x", "x", x_min, x_max)
+    x = ROOT.RooRealVar("x", "m_" + higgs_boson, x_min, x_max)
     x_frame = x.frame(rf.Bins(1))
 
-    canvas = ROOT.TCanvas("canvas", "canvas", 600, 600)
+    title = higgs_boson + " Higgs Boson peak"
+    x_frame.SetTitle(title)
+
+    canvas = ROOT.TCanvas("canvas", "canvas", 1000, 600)
 
     gaus = ROOT.RooBreitWigner("breitwigner", "BreitWigner", x, mean, width)
 
-    for i in xrange(x_width + 1):
+    print " len=", len(m_boson), " len=", len(width_boson)
+    for i in xrange(0, len(m_boson), skipframes):
+        print i
         # create/clone new empty frame
         frame = x_frame.emptyClone(x_frame.GetName() + "_" + str(i))
+
+        frame.SetTitle(title + " (m_A=" + str(m_A[i]) + ")")
 
         mean.setVal(m_boson[i])
         width.setVal(width_boson[i])
 
         # debug output
         if i%10 == 0: print "creating image", i
-        print "mean=", mean.getVal(), "  x=", x.getVal(), "  sigma=", width.getVal()
+        print "mean=", mean.getVal(), "  x=", x.getVal(), "  width=", width.getVal()
 
         # plot normalized gauss function on frame
-        norm = gaus.createIntegral(ROOT.RooArgSet(x), rf.NormSet(ROOT.RooArgSet(x))).getVal()/x_width
+        norm = gaus.createIntegral(ROOT.RooArgSet(x), rf.NormSet(ROOT.RooArgSet(x))).getVal() / (x_max - x_min)
         gaus.plotOn(frame, rf.Normalization(norm, ROOT.RooAbsReal.NumEvent))
 
+        # remove y axis title, set y axis maximum to 1
+        frame.SetYTitle("")
         frame.SetMaximum(1)
+
+        # draw frame
         frame.Draw()
 
         # animation delay in centiseconds (10ms)
-        canvas.Print(filename + "+" + str(int(math.ceil(animation_delay / 10))))
+        canvas.Print(filename + "+" + str(int(math.ceil(animation_delay / 10 ))))
 
     # infinite loop gif
-    canvas.Print(filename + "++" + str(int(math.ceil(animation_delay / 10))) + "++")
-    print filename + " created"
+    canvas.Print(filename + "++100++")
 
 
 if __name__ == '__main__':
