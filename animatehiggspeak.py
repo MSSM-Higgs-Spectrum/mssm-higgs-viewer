a83692de1114b6087ff0f7386ea31ac492260684
Raphael Lehmann <raphael@rleh.de>
2015-09-16 19:05:17 +0200

    add support for plotting multiple datasets

--- animatehiggspeak.py
+++ animatehiggspeak.py
@@ -3,64 +3,103 @@ import os
 import math
 
 
-def animate_higgs_peak(values_mass, values_width, values_ma, higgs_boson, filename="animation.gif", duration=5000,
-                       skipframes=1):
+def calc_x_min(x_list):
+    return int(min(min(l) for l in x_list)) - 5
+
+
+def calc_x_max(x_list):
+    return int(max(max(l) for l in x_list)) + 5
+
+
+def animate_higgs_peak(list_values_mass, list_values_width, values_ma, list_higgs_boson, filename="animation.gif",
+                       duration=5000, skipframes=1):
     # remove gif file (if present)
     try:
         os.remove(filename)
     except OSError:
         pass
 
-    # x axis range (enlarged by 5 (?))
-    x_min = int(min(values_mass)) - 5
-    x_max = int(max(values_mass)) + 5
-
-    # debug output
-    print "x_min =", x_min, "x_max =", x_max
+    num_bosons = len(list_values_mass)
 
     # calculate delay time per frame
     # one interval/frame per dataset
-    animation_delay = math.ceil(duration * skipframes / len(values_mass))
-
-    width = ROOT.RooRealVar("width", "width", 0)
-    mean = ROOT.RooRealVar("mean", "mean", 0)
+    animation_delay = math.ceil(duration * skipframes / len(list_values_mass[0]))
 
     rf = ROOT.RooFit
 
-    x = ROOT.RooRealVar("x", "m_" + higgs_boson, x_min, x_max)
-    x_frame = x.frame(rf.Bins(1))
+    # x axis range (enlarged by 5 (?))
+    x_min = calc_x_min(list_values_mass)
+    x_max = calc_x_max(list_values_mass)
+    x = ROOT.RooRealVar("x", "m", x_min, x_max)
 
-    title = higgs_boson + " Higgs Boson peak"
-    x_frame.SetTitle(title)
+    frame = x.frame(rf.Bins(1))
 
-    canvas = ROOT.TCanvas("canvas", "canvas", 1000, 600)
+    if num_bosons > 1:
+        title = "Higgs bosons peaks"
+    else:
+        title = list_higgs_boson + " Higgs Boson peak"
+    frame.SetTitle(title)
 
+    canvas = ROOT.TCanvas("canvas", "canvas", 1300, 750)
+
+    width = ROOT.RooRealVar("width", "width", 0)
+    mean = ROOT.RooRealVar("mean", "mean", 0)
     gaus = ROOT.RooBreitWigner("breitwigner", "BreitWigner", x, mean, width)
 
-    print " len=", len(values_mass), " len=", len(values_width)
-    for i in xrange(0, len(values_mass), skipframes):
+    if num_bosons > 1:
+        width2 = ROOT.RooRealVar("width", "width", 0)
+        mean2 = ROOT.RooRealVar("mean", "mean", 0)
+        gaus2 = ROOT.RooBreitWigner("breitwigner", "BreitWigner", x, mean2, width2)
+
+    if num_bosons > 2:
+        width3 = ROOT.RooRealVar("width", "width", 0)
+        mean3 = ROOT.RooRealVar("mean", "mean", 0)
+        gaus3 = ROOT.RooBreitWigner("breitwigner", "BreitWigner", x, mean3, width3)
+
+    for i in xrange(0, len(list_values_mass[0]), skipframes):
         print i
         # create/clone new empty frame
-        frame = x_frame.emptyClone(x_frame.GetName() + "_" + str(i))
+        lframe = frame.emptyClone(frame.GetName() + "_" + str(i))
 
-        frame.SetTitle(title + " (m_A=" + str(values_ma[i]) + ")")
+        lframe.SetTitle(title + " (m_A=" + str(values_ma[i]) + ")")
 
-        mean.setVal(values_mass[i])
-        width.setVal(values_width[i])
+        mean.setVal(list_values_mass[0][i])
+        width.setVal(list_values_width[0][i])
 
         # plot normalized gauss function on frame
         norm = gaus.createIntegral(ROOT.RooArgSet(x), rf.NormSet(ROOT.RooArgSet(x))).getVal() / (x_max - x_min)
-        gaus.plotOn(frame, rf.Normalization(norm, ROOT.RooAbsReal.NumEvent))
+        gaus.plotOn(lframe, rf.Normalization(norm, ROOT.RooAbsReal.NumEvent))
 
-        # remove y axis title, set y axis maximum to 1
-        frame.SetYTitle("")
-        frame.SetMaximum(1)
+        if num_bosons > 1:
+            mean2.setVal(list_values_mass[1][i])
+            width2.setVal(list_values_width[1][i])
+            # plot normalized gauss function on frame
+            norm2 = gaus2.createIntegral(ROOT.RooArgSet(x), rf.NormSet(ROOT.RooArgSet(x))).getVal() / (x_max - x_min)
+            gaus2.plotOn(lframe, rf.Normalization(norm2, ROOT.RooAbsReal.NumEvent))
+
+        if num_bosons > 2:
+            mean3.setVal(list_values_mass[2][i])
+            width3.setVal(list_values_width[2][i])
+            # plot normalized gauss function on frame
+            norm3 = gaus3.createIntegral(ROOT.RooArgSet(x), rf.NormSet(ROOT.RooArgSet(x))).getVal() / (x_max - x_min)
+            gaus3.plotOn(lframe, rf.Normalization(norm3, ROOT.RooAbsReal.NumEvent))
+
+        # remove y axis title
+        lframe.SetYTitle("")
+
+        # set y axis maximum to 1
+        lframe.SetMaximum(1)
 
         # draw frame
-        frame.Draw()
+        lframe.Draw()
 
         # animation delay in centiseconds (10ms)
         canvas.Print(filename + "+" + str(int(math.ceil(animation_delay / 10))))
 
     # infinite loop gif
     canvas.Print(filename + "++100++")
+
+
+if __name__ == '__main__':
+    animate_higgs_peak([[10, 20, 40], [7, 15, 24]], [[0.0002, 0.0001, 0.00008], [0.0001, 0.0002, 0.00015]], [60, 70, 80],
+                       ['H', 'h', 'A'], filename='test.gif', duration=1000)
